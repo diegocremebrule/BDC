7c21bdcf24293d74830e937852455dcf070ab57d
Diana Andrade <dianac.andraded@gmail.com>
2025-05-20 12:12:47 +0200

    Add files via upload

---
+++ src/G17HW2.java
@@ -0,0 +1,404 @@
+import org.apache.commons.lang3.tuple.Triple;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+import scala.Tuple2;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+
+public class G17HW2 {
+    public static JavaPairRDD<Vector, Integer> CreateDatasets(JavaRDD<String> rdd){
+        return rdd.mapToPair(line -> {
+            String[] parts = line.split(",");
+            double[] coordinates = new double[parts.length-1];
+            for (int i=0; i<parts.length-1; i++){
+                coordinates[i] = Double.parseDouble(parts[i]);
+            }
+            int group;
+            if (parts[parts.length-1].equals("A")){
+                group = 0;
+            } else {
+                group = 1;
+            }
+            return new Tuple2<Vector,Integer>(Vectors.dense(coordinates),group);
+        });
+    };
+    public static Vector[] Kmeans_C(JavaPairRDD<Vector,Integer> train, int K, int M) {
+        JavaRDD<Vector> points = train.map(point -> {
+            return point._1; //returns the first element in the tuple, which it's the coordinates
+        });
+        KMeansModel model = KMeans.train(points.rdd(), K, M);
+        //Collect centroids in a list of Vectors
+        Vector[] C = new Vector[K];
+        for (int i = 0; i < K; i++) C[i] = model.clusterCenters()[i];
+        return C;
+    }
+    public static JavaRDD<Vector> CalculateDistances(JavaPairRDD<Vector,Integer> U, Vector[] C){
+        JavaRDD<Vector> DCi = U.map(point -> {
+            int My_C = 0;
+            double group = point._2;
+            Vector coords = point._1;
+            double min = Vectors.sqdist(coords, C[0]);
+            for (int i = 0; i < C.length; i++){
+                double dist = Vectors.sqdist(coords, C[i]);
+                if (dist < min){
+                    min = dist;
+                    My_C = i;
+                }
+            }
+            return Vectors.dense(min, My_C, group);
+        });
+        return DCi;
+    }
+    public static double MRComputeFairObjective(JavaRDD<Vector> DCi){
+        JavaRDD<Double> A_Sqdist = DCi.filter(line -> line.apply(2) == 0).map(point -> {
+            double dist = point.apply(0);
+            return dist;
+        });
+        JavaRDD<Double> B_Sqdist = DCi.filter(line -> line.apply(2)==1).map(point -> {
+            double dist = point.apply(0);
+            return dist;
+        });
+        double sumA = A_Sqdist.reduce((a,b) -> a+b);
+        double sumB = B_Sqdist.reduce((a,b) -> a+b);
+        double objA = sumA / A_Sqdist.count();
+        double objB = sumB / B_Sqdist.count();
+        double obj_fair_val = Math.max(objA, objB);
+
+        return obj_fair_val;
+    }
+    public static Vector[] MRFairLloyd(JavaPairRDD<Vector,Integer> U, int K, int M) {
+        JavaRDD<Vector> vector = U.map(p -> p._1); //get a vector of our points, taking out the 1st tuple.
+        KMeansModel model = KMeans.train(vector.rdd(), K, 0); // here we do step1: set {c1,c2,…,ck} of k centroids
+        Vector[] centroids = model.clusterCenters(); //get the coordinates of our centroids
+
+        for (int i = 0; i < M; i++) { // we start for loop until M times
+            Vector[] finalCentroids = centroids;
+            JavaPairRDD<Integer, Tuple2<Vector, Integer>> clusteredPoints = U.mapToPair(coord_label -> {
+                Vector coords = coord_label._1;  // get the coordinates of the points
+                Integer label = coord_label._2;  // get the label (A/B), represented by (0/1)
+                int idx = idx_close(coords, finalCentroids); // get the index of the centroid which is closest to coordinates
+                return new Tuple2<>(idx, new Tuple2<>(coords,label)); //returning this (idx, (coordinates, label))
+            });
+            //add new lists of A and B and inside of them create K lists that we will add the respective points
+            //we return a map: centroid index -> (Vector, Label)
+            Map<Integer, Iterable<Tuple2<Vector, Integer>>> grouped = clusteredPoints.groupByKey().collectAsMap();
+
+            //lists A and B for each cluster (k clusters total)
+            List<List<Vector>> A = new ArrayList<>(K);
+            List<List<Vector>> B = new ArrayList<>(K);
+            for (int j = 0; j < K; j++) {
+                A.add(new ArrayList<>()); //empty lists so cluster goes in position i of A and B respectively
+                B.add(new ArrayList<>());
+
+                //important!! this gets the list of all data points assigned to cluster i
+                Iterable<Tuple2<Vector, Integer>> items = grouped.getOrDefault(j, new ArrayList<>());
+
+                //now we start to iterate over the points in cluster i and then assign the point
+                //to the corresponding cluster list for A and B
+                for (Tuple2<Vector, Integer> pointLabel: items) {
+                    Vector vec = pointLabel._1;
+                    int label = pointLabel._2;
+                    if (label==0) {
+                        A.get(j).add(vec);
+                    } else {
+                        B.get(j).add(vec);
+                    }
+                }
+            }
+            // apply function of centroids in which we give A and B vectors listed with the points for each cluster
+            //0 to k. So A=[cluster1:[(2,3),(4,5)], cluster2:....]
+            //this function is below and chooses the right centroids according to our problem needs.
+            centroids = CentroidSelection(A, B, K);
+        }
+        return centroids;
+    }
+
+    //this function gets the index of the centroid which is closest to coordinates, calculating the sqdist
+        public static int idx_close(Vector coordinates, Vector[] centroids) {
+            double min = Double.MAX_VALUE;
+            int idx = -1;
+
+            for (int i = 0; i < centroids.length; i++) {
+                double dist = Vectors.sqdist(coordinates, centroids[i]);
+
+                if (dist < min) {
+                    min = dist;
+                    idx = i;
+                }
+            }
+
+            return idx;
+        }
+    // Function to compute alpha, beta, MA, MB, and L given groups A, B and number of clusters k for CentroidSelection
+    //we use result that helps us bring multiple results of the same function.
+    public static Result computeVectors(List<List<Vector>> A, List<List<Vector>> B, int K) {
+        // Calculate total number of points in group A and group B
+
+        int total_A = 0;
+        for (List<Vector> clustered_A : A) { // for loop cluster's points in A
+            // Add the number of points in this cluster to the total
+            total_A = total_A + clustered_A.size();
+        }
+
+        int total_B = 0;
+
+        for (List<Vector> clustered_B : B) { //for loop cluster's points in A
+            // Add the number of points in this cluster to the total
+            total_B = total_B + clustered_B.size();
+        }
+
+        double[] alpha = new double[K];; //initialize alpha
+        double[] beta = new double[K]; //initialize beta
+        Vector[] MA = new Vector[K]; //initialize MA
+        Vector[] MB = new Vector[K]; //initialize MB
+        double[] L = new double[K]; //initialize l
+
+        // loop within the clusters
+        for (int i = 0; i < K; i++) {
+            List<Vector> clusterA = A.get(i);
+            List<Vector> clusterB = B.get(i);
+
+            int sizeA = clusterA.size();
+            int sizeB = clusterB.size();
+
+            alpha[i] = (double) sizeA / total_A; //compute alpha
+            beta[i] = (double) sizeB / total_B; //compute beta
+
+            //Compute vector averages for A using function below that calculates these averages per cluster
+            MA[i] = meanVector(clusterA);
+
+            //Compute vector averages for B using function below that calculates these averages per cluster
+            MB[i] = meanVector(clusterB);
+
+            // If MA[i] is null (no points in A for cluster i) -> MA[i] = MB[i]
+            if (MA[i] == null && MB[i] != null) {
+                MA[i] = MB[i];
+            }
+
+            // If MB[i] is null (no points in B for cluster i) -> MB[i] = MA[i]
+            if (MB[i] == null && MA[i] != null) {
+                MB[i] = MA[i];
+            }
+
+            // Euclidean distance to complete L
+            if (MA[i] == null || MB[i] == null) {
+                L[i] = 0; // No distance if centroids undefined
+            } else {
+                L[i] = Math.sqrt(Vectors.sqdist(MA[i], MB[i]));
+            }
+        }
+
+        // Return all vectors need for function Centroid Selection
+        return new Result(alpha, beta, MA, MB, L);
+    }
+
+    //function to compute mean vector of a list of vectors, calculates MA and MB
+    private static Vector meanVector(List<Vector> points) {
+        if (points.isEmpty()) return null;
+
+        int dim = points.get(0).size();
+        double[] sum = new double[dim];
+
+        for (Vector v : points) {
+            double[] arr = v.toArray();
+            for (int j = 0; j < dim; j++) {
+                sum[j] += arr[j];
+            }
+        }
+
+        for (int j = 0; j < dim; j++) {
+            sum[j] /= points.size();
+        }
+
+        return Vectors.dense(sum);
+    }
+
+    //we use result that helps us bring multiple results of the same function. In this case we bring the results
+//of function computeVectors, so we can implement them in CentroidSelection
+    public static class Result {public double[] alpha;public double[] beta;public Vector[] MA;public Vector[] MB;public double[] L;
+
+        public Result(double[] alpha, double[] beta, Vector[] MA, Vector[] MB, double[] L) {
+            this.alpha = alpha;
+            this.beta = beta;
+            this.MA = MA;
+            this.MB = MB;
+            this.L = L;
+        }
+    }
+
+    //Centroid Selection that receives vector A,B, k clusters and moreover receives the computed vectors plus
+//the function that computes Vector x.
+    public static Vector[] CentroidSelection(List<List<Vector>> A, List<List<Vector>> B, int K) {
+        //Step 1 compute vectors
+        Result r = computeVectors(A, B, K);
+        double[] alpha = r.alpha;
+        double[] beta = r.beta;
+        Vector[] MA = r.MA;
+        Vector[] MB = r.MB;
+        double[] L = r.L;
+
+        //step 2 calculate first denominator in fixedA and fixedB
+        int total_A = A.stream().mapToInt(List::size).sum();
+        int total_B = B.stream().mapToInt(List::size).sum();
+
+        //Step 2 calculate first numerator delta(A, MA) and delta(B, MB)
+        double deltaA = 0;
+        for (int i = 0; i < K; i++) {
+            for (Vector a : A.get(i)) {
+                deltaA += Vectors.sqdist(a, MA[i]);
+            }
+        }
+
+        double deltaB = 0;
+        for (int i = 0; i < K; i++) {
+            for (Vector b : B.get(i)) {
+                deltaB += Vectors.sqdist(b, MB[i]);
+            }
+        }
+
+        //Step 2 calculate fixedA and fixedB
+        double fixedA = deltaA / total_A;
+        double fixedB = deltaB / total_B;
+
+        //Step 3 obtain vector x from function
+        Vector X = ComputeVectorX(fixedA, fixedB, alpha, beta, L, K,5);
+
+        //Step 4 obtain centroids
+        Vector[] centroids = new Vector[K];
+        for (int i = 0; i < K; i++) {
+            if (L[i] == 0) {
+                centroids[i] = MA[i];
+            } else {
+                double Xi = X.apply(i);
+                double li = L[i];
+                double coefA = (li - Xi) / li;
+                double coefB = Xi / li;
+
+                double[] MAarr = MA[i].toArray();
+                double[] MBarr = MB[i].toArray();
+                double[] cArr = new double[MA[i].size()];
+
+                for (int d = 0; d < MA[i].size(); d++) {
+                    cArr[d] = coefA * MAarr[d] + coefB * MBarr[d];
+                }
+                centroids[i] = Vectors.dense(cArr);
+            }
+        }
+
+        return centroids;
+    }
+    public static Vector ComputeVectorX(double fixedA, double fixedB, double[] alpha, double[] beta, double[] dist, int K, double T ){
+        double gamma = 0.5;
+        double[] ArrayX = new double[K];
+
+        for (int t=0; t<T; t++){
+            double fAx = fixedA;
+            double fBx = fixedB;
+
+            for (int i=0; i<K; K++){
+                double xi = ((1-gamma)*beta[i]*dist[i])/(gamma*alpha[i]+(1-gamma)*beta[i]);
+                ArrayX[i] = xi;
+                fAx = fAx + alpha[i]*xi*xi;
+                fBx = fBx + beta[i]*(dist[i]-xi)*(dist[i]-xi);
+                if (fAx == fBx){
+                    break;
+                } else{
+                    if (fAx > fBx){
+                        gamma = gamma + Math.pow(0.5, t+1);
+                    } else {
+                        gamma = gamma - Math.pow(0.5, t+1);
+                    }
+                }
+            }
+        }
+        Vector X = Vectors.dense(ArrayX);
+        return X;
+    }
+    public static void main(String[] args){
+
+        //Checking the arguments received through the command line
+        //if (args.length != 4) {
+        //throw new IllegalArgumentException("USAGE: file_path L K M");
+        //}
+
+        //Store arguments into variables from the command line
+        String inputFile = args[0];
+        int L = Integer.parseInt(args[1]);
+        int K = Integer.parseInt(args[2]);
+        int M = Integer.parseInt(args[3]);
+
+        //String inputFile = "uber_small.csv";
+        //int L = 2;
+        //int K = 4;
+        //int M = 20;
+
+        //We start setting up Spark environment
+        SparkConf conf = new SparkConf()
+                .setAppName("KMeansClustering")
+                .setMaster("local[*]")
+                .set("spark.driver.host", "localhost");
+
+        // Initialize JavaSparkContext
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        // Read the data into a string RDD, and subdivides it into L random partitions
+        //Cache is keeping the RDD in memory after the first computation
+        JavaRDD<String> inputPoints = sc.textFile(inputFile).repartition(L).cache();
+
+        //Calculates the number of total points based on the number of lines in inputPoints
+        long N = inputPoints.count();
+        //Filters the lines that end with A and counts the number of points A
+        JavaRDD<String> NArdd = inputPoints.filter(line -> line.endsWith("A"));
+        long NA = NArdd.count();
+        //Computes the number of points B, substracting NA from N
+        int NB = (int)(N-NA);
+
+        //Vector Pair RDD U is created, which contains a tuple with two elements, the first one is the
+        //vector with the coordinates and the second one an integer with the groups represented with zeros
+        //and ones
+        JavaPairRDD<Vector, Integer> U = CreateDatasets(inputPoints);
+
+        //COMPUTATION OF STD CENTERS
+        //U is used to create set C of K centroids using the function Kmeans
+        Vector[] C = Kmeans_C(U, K, M);
+
+        //This function creates an RDD with the distances between the point and the closest cluster
+        //It saves the data with the next structure (DCi, C, Group), where DCi corresponds to the distance
+        //C to the cluster, and group to the group A or B to which the point belongs
+        JavaRDD<Vector> DCi = CalculateDistances(U, C);
+
+        //In this way we will use just one RDD to compute the Std Objective and Fair Objetive function
+        //We'll filter the RDD based on groups for the Fair function
+        //We saved our results in these two variables to use them later during the printing
+        double Obj_std_val = MRComputeFairObjective(DCi);
+
+        //COMPUTATION OF FAIR CENTERS
+        //Computes fair centroids
+        Vector[] C_fair = MRFairLloyd(U,K,M);
+
+        //Calculates min distance
+        JavaRDD<Vector> DCi_fair = CalculateDistances(U, C_fair);
+
+        //Computes Fair Objective Function using fair centroids
+        double Obj_fair_std_val = MRComputeFairObjective(DCi_fair);
+
+        //Prints the outputs in the requested format
+        System.out.println("Input file path = " + inputFile + ", L = " + L + ", K = " + K + ", M = " + M);
+        System.out.println("N = " + N + ", NA = " + NA + ", NB = " + NB);
+        System.out.println("Fair Objective with Standard Centers = " + Obj_std_val);
+        System.out.println("Fair Objective with Fair Centers = " + Obj_fair_std_val);
+        System.out.println("Time to compute Standard Centers = ");
+        System.out.println("Time to compute Fair Centers = ");
+        System.out.println("Time to compute Objective with Standard Centers = ");
+        System.out.println("Time to compute Objective with Fair Centers = ");
+
+        sc.close();
+    }
+}
